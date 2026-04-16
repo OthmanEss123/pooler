3aab055ffe88c9a09c5b2d139bbd9d0c7edda12a
OthmanEss123 <othmanessaadi4@gmail.com>
2026-04-16 18:01:46 +0100

    fix: accept short date format in analytics dto

--- src/modules/analytics/dto/query-analytics.dto.ts
+++ src/modules/analytics/dto/query-analytics.dto.ts
@@ -1,10 +1,25 @@
-import { IsDateString, IsEnum, IsOptional, IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 
 export class QueryAnalyticsDto {
-  @IsDateString()
+  @IsString()
+  @Transform(({ value }) => {
+    if (!value) return value;
+    if (/^\d{4}-\d{2}-\d{2}$/.test(value)) {
+      return `${value}T00:00:00.000Z`;
+    }
+    return value;
+  })
   from: string;
 
-  @IsDateString()
+  @IsString()
+  @Transform(({ value }) => {
+    if (!value) return value;
+    if (/^\d{4}-\d{2}-\d{2}$/.test(value)) {
+      return `${value}T23:59:59.000Z`;
+    }
+    return value;
+  })
   to: string;
 
   @IsOptional()
